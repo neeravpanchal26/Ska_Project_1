d9fc21d42db5722a8b72dc699d9c1c9ad70386ad
Neerav Panchal <masterpanchieneerav@gmail.com>
2020-02-27 10:58:29 +0200

    Elastic Search
    - Elastic search service done
    - To fix CORS with Angular proxy

--- src/app/Components/default/default.component.ts
+++ src/app/Components/default/default.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
+
+// Custom imports
+import {ElasticSearchService} from '../../GlobalServices/elastic-search.service';
 
 @Component({
   selector: 'app-default',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
+  // Global variable
+  private isConnected = false;
+  private status: string;
 
-  constructor() { }
+  // Default constructor
+  constructor(private es: ElasticSearchService, private cd: ChangeDetectorRef) {
+  }
 
+  // Form load
   ngOnInit() {
+    this.es.isAvailable().then(() => {
+      this.status = 'OK';
+      this.isConnected = true;
+    }, error => {
+      this.status = 'ERROR';
+      this.isConnected = false;
+      console.error('Server is down', error);
+    }).then(() => {
+      this.cd.detectChanges();
+    });
   }
 
 }
